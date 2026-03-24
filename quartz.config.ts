27129d04fda66c7ee644c28baacfd1a378833ede
Alexender Yatsenko <ayatsenkok@gmail.com>
2026-03-24 02:45:15 +0000

    change font to @fontsource/maple-mono

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,12 +20,12 @@ const config: QuartzConfig = {
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
-      fontOrigin: "googleFonts",
+      fontOrigin: "local",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Maple Mono",
+        body: "Maple Mono",
+        code: "Maple Mono",
       },
       colors: {
         lightMode: {
